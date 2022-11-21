730e384c33ff25682407435b08e525c634e54654
Dongeun Paeng <dongeun.paeng@gmail.com>
2022-11-21 17:25:44 +0900

    feat: 글 수정, 조회 api 추가

--- src/main/java/com/dongeunpaeng/springblog/service/posts/PostsService.java
+++ src/main/java/com/dongeunpaeng/springblog/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.dongeunpaeng.springblog.service.posts;
 
+import com.dongeunpaeng.springblog.domain.posts.Posts;
 import com.dongeunpaeng.springblog.domain.posts.PostsRepository;
+import com.dongeunpaeng.springblog.web.dto.PostsResponseDto;
 import com.dongeunpaeng.springblog.web.dto.PostsSaveRequestDto;
+import com.dongeunpaeng.springblog.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,17 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
     }
 }
