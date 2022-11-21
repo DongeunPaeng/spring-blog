9b026a0e6170a3ba7db5c4fed137c3f130a0951c
Dongeun Paeng <dongeun.paeng@gmail.com>
2022-11-21 22:19:51 +0900

    feat: Google OAuth2 구현 중(미완)

---
+++ src/main/java/com/dongeunpaeng/springblog/config/auth/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.dongeunpaeng.springblog.config.auth;
+
+import com.dongeunpaeng.springblog.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+// TODO: 여기부터 이어서 하기
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                        .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll().logoutSuccessUrl("/")).oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+
+        ;
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
